559a6bad32187042d46092805afc80592ee30c50
Ben <thebillham@gmail.com>
2020-12-21 17:17:21 +1300

    Linked to WordPress blog

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
-import Head from 'next/head'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+import Head from "next/head";
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { getAllPostsForHome } from "../lib/api";
+import { CMS_NAME } from "../lib/constants";
 
-export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
+export default function Index({ allPosts: { edges } }) {
+  const heroPost = edges[0]?.node;
+  const morePosts = edges.slice(1);
 
   return (
     <>
-      <Layout preview={preview}>
+      <Layout>
         <Head>
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
@@ -33,12 +33,15 @@ export default function Index({ allPosts: { edges }, preview }) {
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
+export async function getStaticProps() {
+  // console.log(preview);
+  const allPosts = await getAllPostsForHome();
   return {
-    props: { allPosts, preview },
-  }
+    props: {
+      allPosts,
+    },
+  };
 }
